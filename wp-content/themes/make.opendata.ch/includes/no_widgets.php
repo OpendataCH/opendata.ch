04221fd22ee9316b8058e542fe6958f3ea57c5ca
Fabian Vogler <fabian@equivalence.ch>
2016-07-21 15:04:55 +0200

    Remove make.opendata.ch theme
    Use Opendata.ch theme with custom CSS for colors

--- wp-content/themes/make.opendata.ch/includes/no_widgets.php
+++
@@ -1,3 +0,0 @@
-<h3>Widget Area</h3>
-<p>This is an area for widgets. You can add and edit widgets from the WordPress admin.</p>				 
-<p><a class="action" href="<?php bloginfo('url'); ?>/wp-admin/widgets.php">Customize Widgets</a></p>
