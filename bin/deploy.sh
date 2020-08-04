1b128af530e9a8417ac5c104cc04dcb4207e0189
Fabian Vogler <fabian@equivalence.ch>
2020-08-04 20:49:20 +0200

    Remove Travis CI

--- bin/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh -o ForwardAgent=yes www-data@opendata02.nine.ch "cd /home/www-data/opendata.ch && git pull --rebase --stat && /home/www-data/bin/composer update --no-dev --no-interaction --no-progress --optimize-autoloader"
