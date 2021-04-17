4537e037b19ae286ac315770f3b633eda02fd414
Yacine Belarbi <yacbel@gmail.com>
2021-04-17 11:26:38 +0200

    added node-modules gitignore, fixes

--- wp-content/themes/studioyacine/library/functions/admin.php
+++ wp-content/themes/studioyacine/library/functions/admin.php
@@ -9,15 +9,15 @@ function remove_menus(){
 	remove_menu_page( 'edit-comments.php' );          //Comments
 
 	if ( current_user_can( 'editor' ) ) {
-	  //remove_menu_page( 'index.php' );                  //Dashboard
-	  //remove_menu_page( 'jetpack' );                    //Jetpack*
-	  //remove_menu_page( 'upload.php' );                 //Media
+	  remove_menu_page( 'index.php' );                  //Dashboard
+	  remove_menu_page( 'jetpack' );                    //Jetpack*
+	  remove_menu_page( 'upload.php' );                 //Media
 	  // remove_menu_page( 'edit.php?post_type=page' );    //Pages
-	  //remove_menu_page( 'themes.php' );                 //Appearance
-	  //remove_menu_page( 'plugins.php' );                //Plugins
-	  //remove_menu_page( 'users.php' );                  //Users
-	  //remove_menu_page( 'tools.php' );                  //Tools
-	  //remove_menu_page( 'options-general.php' );        //Settings
+	  remove_menu_page( 'themes.php' );                 //Appearance
+	  remove_menu_page( 'plugins.php' );                //Plugins
+	  remove_menu_page( 'users.php' );                  //Users
+	  remove_menu_page( 'tools.php' );                  //Tools
+	  remove_menu_page( 'options-general.php' );        //Settings
 	}
 
 }
