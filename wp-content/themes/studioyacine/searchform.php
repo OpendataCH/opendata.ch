0506d7477216e3f77630930395673ec8ae81bdf4
Yacine Belarbi <yacbel@gmail.com>
2021-07-06 09:57:53 +0200

    searchform

--- wp-content/themes/studioyacine/searchform.php
+++ wp-content/themes/studioyacine/searchform.php
@@ -1,5 +1,7 @@
 <form role="search" method="get" class="searchform" action="<?php echo home_url('/'); ?>">
 
+    <label class='visuallyhidden' for="s">Search</label>
+    
     <input type="search" id="s" name="s" placeholder='Enter a search term' value="" />
 
     <button type="submit" id="searchsubmit"><?php _e('Search', 'bonestheme'); ?></button>
