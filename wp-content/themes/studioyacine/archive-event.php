f48c06b0c967bd356e8fd8857c2c399ee13eac48
Yacine Belarbi <yacbel@gmail.com>
2022-08-09 16:14:16 +0200

    no events fix

--- wp-content/themes/studioyacine/archive-event.php
+++ wp-content/themes/studioyacine/archive-event.php
@@ -77,14 +77,8 @@
 
 			<article id="post-not-found" class="hentry ">
 				<header class="article-header">
-					<h1><?php _e('Oops, Post Not Found!', 'bonestheme'); ?></h1>
+					<h1><?php _e('Oh, no events.', 'bonestheme'); ?></h1>
 				</header>
-				<section class="entry-content">
-					<p><?php _e('Uh Oh. Something is missing. Try double checking things.', 'bonestheme'); ?></p>
-				</section>
-				<footer class="article-footer">
-					<p><?php _e('This is the error message in the archive.php template.', 'bonestheme'); ?></p>
-				</footer>
 			</article>
 
 		<?php endif; ?>
